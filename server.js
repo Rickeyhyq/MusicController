12cd43ff2256cb7224945069b3d5b75ba9d98433
Ricky <heyuqi2681@hotmail.com>
2017-03-10 20:47:40 +0800

    添加模板页

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ const http = require('http'),
       // 引入第三方包
       xtpl = require('xtpl'),
       // 引入音乐数据(转为了js对象)
-      musics = require('./musics.json')
+      datas = require('./musics.json')
 
 const server = http.createServer()
 server.on('request', (req, res) => {
   const urlString = req.url
   if (urlString === '/' || urlString.includes('index.html')) {
-
     console.log(urlString)
+    xtpl.renderFile(path.join(__dirname, 'index.html'), {
+      musics: datas
+    }, (error, data) => {
+      let htmlContent = data.toString()
+      
+    })
     res.setHeader('Content-Type', 'text/html;charset=utf-8')
     res.end('Hello World')
   }
