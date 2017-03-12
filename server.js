a8353bfd7383315e6d1fffa2a1d76ce6c8406331
Ricky <heyuqi2681@hotmail.com>
2017-03-12 20:35:22 +0800

    使用express改进路由功能

--- server.js
+++ server.js
@@ -1,52 +1,35 @@
 // 加载依赖包
-const http = require('http'),
-      fs = require('fs'),
-      path = require('path'),
-      // 引入第三方包
-      xtpl = require('xtpl'),
-      // 引入音乐数据(转为了js对象)
-      datas = require('./musics.json'),
-      // 引入query string 模块解决 url编码问题
-      querystring = require('querystring')
-const server = http.createServer()
-server.on('request', (req, res) => {
-  let urlString = req.url
-  // 根据请求地址判断请求的类型
-  if (urlString === '/' || urlString.includes('index.html')) {
-    console.log(urlString)
-    // 获取数据渲染模板页
-    xtpl.renderFile(path.join(__dirname, 'index.html'), {
-      musics: datas
-    }, (error, data) => {
-      // 将渲染好的页面返回给浏览器
-      let htmlContent = data.toString()
-      
-      res.setHeader('Content-Type', 'text/html;charset=utf-8')
-      res.end(htmlContent)
-    })
-  } else if (urlString.includes('music.css')) {
-    fs.readFile(path.join(__dirname, 'statics/css/music.css'), (error, data) => {
-      res.setHeader('Content-Type', 'text/css;charset=utf-8')
-      res.end(data.toString())
-    })
-  } else if (urlString.includes('jquery')) {
-    fs.readFile(path.join(__dirname, 'statics/js/jquery.min.js'), (error, data) => {
-      res.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-      res.end(data.toString())
-    })
-  } else if (urlString.includes('mp3')) {
-    // 解决post请求中文路径url百分比编码问题
-    urlString = querystring.unescape(urlString)
-    fs.readFile(path.join(__dirname, 'statics/music' + urlString), (error, data) => {
+const express = require('express'),
+  fs = require('fs'),
+  path = require('path'),
+  // 引入第三方包
+  xtpl = require('xtpl'),
+  // 引入音乐数据(转为了js对象)
+  datas = require('./musics.json'),
+  // 引入query string 模块解决 url编码问题
+  querystring = require('querystring'),
+  app = express(),
+  router = express.Router()
 
-      res.setHeader('Content-Type', 'audio/mpeg;charset=utf-8')
-      // 音频文件文二进制文件，不需要转成字符串格式
-      res.end(data)
-    })
-  }
+app.use(express.static(path.join(__dirname, 'statics')))
+
+// app.use('/music', router)
+
+const deal = (req, res) => {
+  xtpl.renderFile(path.join(__dirname, 'index.html'), { musics: datas }, (err, content) => {
+    res.send(content);
+  })
+}
+
+app.all('/', (req, res, next) => {
+  deal(req, res)
 })
 
-server.listen(5000, '127.0.0.1', (error) => {
+app.get('/index.html', (req, res) => {
+  deal(req, res)
+})
+
+app.listen(7000, '127.0.0.1', (error) => {
   if (error) {
     console.log(error)
   }
